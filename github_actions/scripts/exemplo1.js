f49be37ccba3b5ae0b79c4687ae8a39fb509b426
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2024-04-21 14:44:37 -0300

    aulas bonus

---
+++ github_actions/scripts/exemplo1.js
@@ -0,0 +1,29 @@
+// Utilizando o Github Actions para K6
+
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export const options = {
+  stages: [
+    { duration: "10s", target: 10 },
+    { duration: "10s", target: 10 },
+    { duration: "10s", target: 0 },
+  ],
+  thresholds: {
+    checks: ["rate>0.95"],
+    http_req_duration: ["p(95)<200"],
+  },
+  ext: {
+    loadimpact: {
+      projectId: "3692905",
+      name: "POC CURSO K6",
+    },
+  },
+};
+
+export default function () {
+  const BASE_URL = "http://test-api.k6.io/public/crocodiles/";
+  let res = http.get(BASE_URL);
+  check(res, { "status is 200": (r) => r.status === 200 });
+  sleep(1);
+}
